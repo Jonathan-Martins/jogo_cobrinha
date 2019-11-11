157366e48f42d4eb34779a40cdc047cba118740b
Jonathan Martins <jonathan.santosmartins@gmail.com>
2019-11-11 16:52:36 -0300

    adicionado mecanica de colisao e aviso de fim de jogo

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ const food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
+// Função para criar o fundo do jogo
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+// Função para desenhar a cobra
 function criarCobrinha() {
     for(i=0; i < snake.length; i++) {
         context.fillStyle = "green";
@@ -24,6 +26,7 @@ function criarCobrinha() {
     }
 }
 
+// Função para desenhar a fruta, comida.
 function drawFood() {
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
@@ -39,31 +42,52 @@ function update(event) {
 }
 
 function iniciarJogo() {
+    // Define as condições para que a cobra ao sair por um lado apareça do outro
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    // Verificando se a cobra se chocou com ela mesma
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over :(")
+        }
+    }
+
+    // Funções de inicialização do jogo
     criarBG();
     criarCobrinha();
     drawFood();
-    
+
+    // Variaveis para a posicão da cobra no plano cartesiano
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    // Define as direções
     if (direction == "right") snakeX += box;
     if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
-
+    // Define o aumento da cobra ao comer a fruta
+    if (snakeX != food.x|| snakeY != food.y){
+        snake.pop();
+    } 
+    else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+    
+    // Obj com as coordenadas da nova cabeça da cobra
     let newHead = {
         x: snakeX,
         y: snakeY
     }
-
+    // Adiciona um novo elemento no ínicio do array
     snake.unshift(newHead);
 }
 
+// intervalo de em que a função inicia jogo é executada. 100ms
 const jogo = setInterval(iniciarJogo, 100)
